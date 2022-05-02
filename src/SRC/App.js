bc7866d8300dac9ef06ff45d557b4aceee0c480f
mlar227 <104795810+mlar227@users.noreply.github.com>
2022-05-02 14:04:32 -0700

    Delete src directory

--- src/SRC/App.js
+++
@@ -1,8 +0,0 @@
-
-    import {Mobile1} from "./Mobile1";
-
-    export default function App() {
-        document.body.style.backgroundColor = '#f2f2f2';
-        document.body.style.margin = '0';
-        return <Mobile1 />;
-    }
