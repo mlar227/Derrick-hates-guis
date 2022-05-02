4dbd4e9f0e5d1afe1433009d83e00831bde9b470
mlar227 <104795810+mlar227@users.noreply.github.com>
2022-05-02 13:55:09 -0700

    Initial commit

---
+++ src/SRC/App.js
@@ -0,0 +1,8 @@
+
+    import {Mobile1} from "./Mobile1";
+
+    export default function App() {
+        document.body.style.backgroundColor = '#f2f2f2';
+        document.body.style.margin = '0';
+        return <Mobile1 />;
+    }
